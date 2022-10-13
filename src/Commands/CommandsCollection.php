ce99848b052132f7aa1c583b950a4ee6aea397ec
Gabriel Solomon <hello@gabrielsolomon.ro>
2022-10-13 12:54:39 +0300

    fix: deprecation 8.1

--- src/Commands/CommandsCollection.php
+++ src/Commands/CommandsCollection.php
@@ -7,6 +7,7 @@ use ArrayAccess;
 use Exception;
 use Iterator;
 use Nip\Collections\Traits\ArrayAccessTrait;
+use Nip\Collections\Traits\SortingTrait;
 
 /**
  * Class CommandsStack
@@ -15,6 +16,7 @@ use Nip\Collections\Traits\ArrayAccessTrait;
 class CommandsCollection implements ArrayAccess, Iterator
 {
     use ArrayAccessTrait;
+    use SortingTrait;
 
     protected $items = [];
 
@@ -88,61 +90,5 @@ class CommandsCollection implements ArrayAccess, Iterator
     {
         unset($this->items[$offset]);
         $this->hops = count($this);
-    }
-
-    /**
-     * Return the current element
-     * @link http://php.net/manual/en/iterator.current.php
-     * @return mixed Can return any type.
-     * @since 5.0.0
-     */
-    public function current()
-    {
-        // TODO: Implement current() method.
-    }
-
-    /**
-     * Move forward to next element
-     * @link http://php.net/manual/en/iterator.next.php
-     * @return void Any returned value is ignored.
-     * @since 5.0.0
-     */
-    public function next()
-    {
-        // TODO: Implement next() method.
-    }
-
-    /**
-     * Return the key of the current element
-     * @link http://php.net/manual/en/iterator.key.php
-     * @return mixed scalar on success, or null on failure.
-     * @since 5.0.0
-     */
-    public function key()
-    {
-        // TODO: Implement key() method.
-    }
-
-    /**
-     * Checks if current position is valid
-     * @link http://php.net/manual/en/iterator.valid.php
-     * @return boolean The return value will be casted to boolean and then evaluated.
-     * Returns true on success or false on failure.
-     * @since 5.0.0
-     */
-    public function valid()
-    {
-        // TODO: Implement valid() method.
-    }
-
-    /**
-     * Rewind the Iterator to the first element
-     * @link http://php.net/manual/en/iterator.rewind.php
-     * @return void Any returned value is ignored.
-     * @since 5.0.0
-     */
-    public function rewind()
-    {
-        // TODO: Implement rewind() method.
     }
 }
